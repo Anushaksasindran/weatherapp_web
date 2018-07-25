0036d7b058aee00abc925e257082ca5ba1a9e10d
anushaksasindran <anushaksasindran@gmail.com>
2018-07-25 12:38:38 +0530

    Third commit

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/', function (req, res) {
     }
   });
 })
-const PORT = process.env.PORT || 3000; 
-app.listen(3000, function () {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!')
 })
